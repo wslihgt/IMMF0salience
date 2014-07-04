ce31d9d957e64337365fca57e361960a8c0ac8c6
Jean-Louis Durrieu <jean-louis@durrieu.ch>
2014-07-04 18:17:03 +0200

    precalc: doing same for another wf0 basis

--- f0Salience.cpp
+++ f0Salience.cpp
@@ -31,7 +31,7 @@
 // computed may be seen - this should be fixed, if one finds the time
 // to do so...
 #include "precalc/wf0_50_600_16000_512_16_05.h"
-#include "wf0_50_600_44100_2048_16_05.h" 
+#include "precalc/wf0_50_600_44100_2048_16_05.h" 
 #include "wf0_100_800_44100_2048_8_05.h" 
 #include "wf0_100_800_16000_2048_16_05.h"
 #include "wf0_100_800_44100_2048_16_05.h"
@@ -607,16 +607,6 @@ F0Salience::genBaseWF0(float Fm, float FM, float inputSamplingRate,
       // cout << N_notes*Nf << endl;
       cout << "Loading from pre-computed Matrices" << endl;
       writeWF0_50_600_16000_512_16_05(f0Table, WF0, (int) N_notes);
-      /*
-	for(ff=0;ff<N_notes-1;ff++)
-	{
-	  f0Table[ff] = F0Table_50_600_16000_512_16_05[ff];
-	  for(F=0;F<Nf;F++)
-	    {
-	      WF0[ff*Nf+F] = WF0_50_600_16000_512_16_05[ff*Nf+F] + eps;
-	    }
-	}
-      */
       // last element actually noise:
       for(F=0;F<Nf;F++)
 	{
@@ -633,14 +623,7 @@ F0Salience::genBaseWF0(float Fm, float FM, float inputSamplingRate,
       // cout << "Total number in WF0 should be 707250, is ";
       // cout << N_notes*Nf << endl;
       cout << "Loading from pre-computed Matrices" << endl;
-      for(ff=0;ff<N_notes-1;ff++)
-	{
-	  f0Table[ff] = F0Table_50_600_44100_2048_16_05[ff];
-	  for(F=0;F<Nf;F++)
-	    {
-	      WF0[ff*Nf+F] = WF0_50_600_44100_2048_16_05[ff*Nf+F] + eps;
-	    }
-	}
+      writeWF0_50_600_44100_2048_16_05(f0Table, WF0, (int) N_notes);
       
       // last element actually noise:
       for(F=0;F<Nf;F++)
---
+++ precalc/wf0_50_600_44100_2048_16_05.h
@@ -0,0 +1,13 @@
+/*
+  some pre-computed matrices for vamp plug-in
+  Would a better way be to load these from some file instead?
+*/
+
+#ifndef _WF0_50_600_44100_2048_16_05_H_ 
+#define _WF0_50_600_44100_2048_16_05_H_
+
+void writeWF0_50_600_44100_2048_16_05(float *F0Table,
+				      float *WF0,
+				      int n_notes);
+
+#endif
